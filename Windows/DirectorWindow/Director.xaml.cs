bea1ed9e2ddcc71e87f23c81ea0e777b95f06473
Forel0ck <tysim190@gmail.com>
2023-02-02 14:20:05 +0300

    Добавьте файлы проекта.

---
+++ Windows/DirectorWindow/Director.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISP918VB_FEDIN_NESMELOV.Windows.DirectorWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для Director.xaml
+    /// </summary>
+    public partial class Director : Page
+    {
+        public Director()
+        {
+            InitializeComponent();
+        }
+    }
+}
